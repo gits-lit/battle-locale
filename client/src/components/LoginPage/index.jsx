c694fa9dcebf141ff6ca1799092df9c8f9794dae
chanhenry54 <hechan@ucsd.edu>
2021-01-16 11:05:45 -0800

    started work on login/queue, added ant design

--- client/src/components/LoginPage/index.jsx
+++ client/src/components/LoginPage/index.jsx
@@ -3,9 +3,20 @@ import React from 'react';
 import './style.scss';
 
 const LoginPage= () => {
+  const handleUserLogin = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="login-page">
       <h1>Login Page</h1>
+      <div className="username-form">
+        <form onSubmit={handleUserLogin}>
+          <label htmlFor="username">Username</label>
+          <input id="username" name="username" required></input>
+          <input type="submit" value="Play"></input>
+        </form>
+      </div>
     </div>
   );
 };
--- client/src/components/QueuePage/index.jsx
+++ client/src/components/QueuePage/index.jsx
@@ -1,11 +1,57 @@
 import React from 'react';
+import { UserOutlined } from '@ant-design/icons';
 
 import './style.scss';
 
 const QueuePage= () => {
+  const checkPlayers = () => {
+    let value = 3 + 3;
+    // let numPlayers = API.getUsers().length;
+    return (
+      <div className="live-queue">
+        <div className="player-row-one">
+          <UserOutlined />
+          <UserOutlined />
+          <UserOutlined />
+          <UserOutlined />
+          <UserOutlined />
+        </div>
+        <div className="player-row-two">
+          <UserOutlined />
+          <UserOutlined />
+          <UserOutlined />
+          <UserOutlined />
+          <UserOutlined />
+        </div>
+        <p className="players-found-text"><span>{value}</span> out of 10 players found</p>
+      </div>
+    );
+  };
+
   return (
     <div className="queue-page">
       <h1>Queue Page</h1>
+      <div className="queue">
+        <p className="queue-text">in queue...</p>
+        <div className="player-row-one">
+          <UserOutlined />
+          <UserOutlined />
+          <UserOutlined />
+          <UserOutlined />
+          <UserOutlined />
+        </div>
+        <div className="player-row-two">
+          <UserOutlined />
+          <UserOutlined />
+          <UserOutlined />
+          <UserOutlined />
+          <UserOutlined />
+        </div>
+        <p className="players-found-text">{checkPlayers()} out of 10 players found</p>
+      </div>
+      <div className="exit-link">
+        <a className="exit-queue" href="/login">exit queue</a>
+      </div>
     </div>
   );
 };
