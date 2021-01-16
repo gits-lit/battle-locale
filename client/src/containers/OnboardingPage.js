9353a9be422be3db7d339b7491f26e01d2db16fa
Emily Nguyen <emily@computer.org>
2021-01-16 09:45:34 -0800

    Fix Onboarding imports

---
+++ client/src/containers/OnboardingPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import OnboardingPage from '../components/LoadingPage';
+
+const OnboardingPageContainer = (props) => {
+  return (
+    <OnboardingPage/>
+  );
+};
+
+export default OnboardingPageContainer;
