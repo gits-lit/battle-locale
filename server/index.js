608a0847dd7f0eb3a4e77cfd7e665fe80788e4fe
Jeffrey Ha <jha@ucsd.edu>
2021-01-16 01:07:53 -0800

    added routes

--- server/index.js
+++ server/index.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
-// load in environment variables
-require('dotenv').config();
-
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// to avoid an CORS issue
+// to avoid a CORS issue
 app.use((req, res, next) => {
     // http://localhost:3000
     res.header("Access-Control-Allow-Origin", "*");
@@ -17,7 +13,12 @@ app.use((req, res, next) => {
     next();
 });
 
+// just some default page
 app.get('/', (req, res) => res.send("Battle Locale's server :)"));
+
+// set up the API routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/game', require('./routes/game'));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}.`));
