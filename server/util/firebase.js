7584c40c85cc297e09ad496035b1fd4bbf0d0361
Jeffrey Ha <jha@ucsd.edu>
2021-01-16 22:50:18 -0800

    added more creds

--- server/util/firebase.js
+++ server/util/firebase.js
@@ -4,7 +4,8 @@ const admin = require('firebase-admin');
 // load in environment variables
 require('dotenv').config();
 
-const config = {
+// load in our app's config
+firebase.initializeApp({
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.FIREBASE_DATABASE_URL,
@@ -12,11 +13,16 @@ const config = {
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.FIREBASE_APP_ID
-};
+});
 
-// load in our app's config
-firebase.initializeApp(config);
-admin.initializeApp(config);
+admin.initializeApp({
+    credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_ADMIN_PRIV_KEY.replace(/\\n/g, '\n')
+    }),
+    databaseURL: process.env.FIREBASE_ADMIN_DATABASE_URL
+});
 
 // export the database service
 module.exports = {
