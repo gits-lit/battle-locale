0b1debff9478e6be9457c4c6fae25f32642d1ade
Jeffrey Ha <jha@ucsd.edu>
2021-01-16 18:57:09 -0800

    moved constants to here

---
+++ server/util/constants/spells.js
@@ -0,0 +1,1 @@
+module.exports = ["arcane-shot", "fireball", "ice-bolt", "heal"];
---
+++ server/util/constants/spelltomes.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "id": {
+        lat: 21.738836,
+        long: -97.933290,
+        spell: undefined // will be assigned upon game start
+    }
+};
