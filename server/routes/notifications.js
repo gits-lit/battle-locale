98ba3a0e4516998f1c6abeaebfaa396586681599
Jeffrey Ha <jha@ucsd.edu>
2021-01-16 18:57:33 -0800

    added how-to connect on client comments

--- server/routes/notifications.js
+++ server/routes/notifications.js
@@ -1,8 +1,26 @@
 const router = require('express')();
-const { messaging } = require('../util/firebase');
+const f = require('../util/system');
+const { messaging: msg } = require('../util/firebase');
 
+/*
+    Client-side: https://firebase.google.com/docs/cloud-messaging/js/send-multiple
+        -> Shows how to retrieve the client's token so you can send it to the server
+        -> https://firebase.google.com/docs/cloud-messaging/js/receive
+            -> After subscribing, on client-side, this shows how to connect to messages / notifications.
+*/
 router.post('/subscribe/', async (req, res) => {
+    let token = req.body.token;
 
+    if (token && token.length > 0) {
+        try {
+            await msg.subscribeToTopic(token, 'game');
+            res.json(f.createSuccess());
+        } catch (e) {
+            res.json(f.createError(`Error in subscribing client to topic: ${e.message}`));
+        }
+    } else {
+        res.json(f.createError('Please provide a valid client token.'));
+    }
 });
 
 module.exports = router;
