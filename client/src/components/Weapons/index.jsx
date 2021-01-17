56bb76b867806929575a553f80f2bae02fc92c12
leestanley <stanleylee91776@gmail.com>
2021-01-17 01:45:29 -0800

    meow

--- client/src/components/Weapons/index.jsx
+++ client/src/components/Weapons/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import WeaponSlot from '../../assets/WeaponSlot.png';
 import WeaponData from '../../weapons.json';
@@ -6,28 +6,36 @@ import WeaponData from '../../weapons.json';
 import './style.scss';
 
 const Weapons = (props) => {
+  const [weapons, setWeapons] = useState([
+    'arcane',
+    'fireball',
+    null,
+    null,
+    null,
+  ]);
+
+  const listItems = weapons.map((weapon) => <WeaponSlots weapon={weapon} />);
+
   return (
     <div className="weapons">
-      <div className="weapons-row">
-        <WeaponSlots weapon="arcane" />
-        <WeaponSlots weapon="arcane" />
-        <WeaponSlots weapon="arcane" />
-        <WeaponSlots weapon="arcane" />
-        <WeaponSlots weapon="arcane" />
-      </div>
+      <div className="weapons-row">{listItems}</div>
     </div>
   );
 };
 
 const WeaponSlots = (props) => {
+  let data = props.weapon;
+  let active = false;
+  if (props.weapon != null) {
+    active = true;
+  }
+
   return (
     <div className="weapon-slot">
-      {WeaponData.map((item) => (
-        <div>
-          <img src={props.weapon.item.image} alt="weapon" className="weapon" />
-          <img src={WeaponSlot} alt="weapon" className="weapon" />
-        </div>
-      ))}
+      {active ? (
+        <img src={WeaponData[data].image} alt="weapon" className="weapon" />
+      ) : null}
+      <img src={WeaponSlot} alt="weapon" className="slot" />
     </div>
   );
 };
